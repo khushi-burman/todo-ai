e7e5382b4829e0c608278ee692e5fff62a8233d6
khushi-burman <khushiiix077@gmail.com>
2026-05-28 14:41:47 +0530

    Day 1: Set up Spring Boot enterprise engine and Task model

---
+++ src/test/java/com/smarttask/todo_ai/TodoAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarttask.todo_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
